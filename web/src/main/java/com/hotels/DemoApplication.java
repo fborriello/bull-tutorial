5dc3d4cb186d9b8b4420029d34734f35cc5f2cfd
Fabio Borriello <borriello.fabio@gmail.com>
2019-05-19 12:05:08 +0200

    removed not needed import

--- web/src/main/java/com/hotels/DemoApplication.java
+++ web/src/main/java/com/hotels/DemoApplication.java
@@ -2,13 +2,11 @@ package com.hotels;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
     public static void main(String[] args) {
