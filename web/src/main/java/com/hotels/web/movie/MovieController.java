be1966ab74d24fa061b3794f8bad4affe73f109c
Fabio Borriello <borriello.fabio@gmail.com>
2019-05-19 10:46:03 +0200

    - Added test for searchEndpoint - Configured in order to run with java 12

--- web/src/main/java/com/hotels/web/movie/MovieController.java
+++ web/src/main/java/com/hotels/web/movie/MovieController.java
@@ -8,7 +8,6 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.util.Assert.notNull;
 
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
