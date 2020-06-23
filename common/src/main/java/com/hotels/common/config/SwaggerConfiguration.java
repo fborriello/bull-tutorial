5eeeb8fcdca21df3c8f4e21ca5d36d839fa74847
Fabio Borriello <fborriello@expedia.com>
2020-06-23 07:34:51 +0200

    Replaces the no longer existing swagger annotation: @EnableSwagger2

--- common/src/main/java/com/hotels/common/config/SwaggerConfiguration.java
+++ common/src/main/java/com/hotels/common/config/SwaggerConfiguration.java
@@ -14,13 +14,13 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 /**
  * Swagger configuration.
  */
 @Configuration
-@EnableSwagger2
+@EnableSwagger2WebMvc
 public class SwaggerConfiguration {
 
     @Value("${swagger.enabled}")
