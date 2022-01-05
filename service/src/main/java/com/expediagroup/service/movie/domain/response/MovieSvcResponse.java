9272da0957ad5005c6763950ca661126d2f5d267
Fabio Borriello <borriello.fabio@gmail.com>
2022-01-05 10:49:38 +0100

    JDK17 upgrade (#181)
    Upgrades the project to jdk 17

--- service/src/main/java/com/expediagroup/service/movie/domain/response/MovieSvcResponse.java
+++ service/src/main/java/com/expediagroup/service/movie/domain/response/MovieSvcResponse.java
@@ -4,17 +4,10 @@ import java.util.List;
 
 import com.expediagroup.common.constants.Genre;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+public record MovieSvcResponse(String id, String title, String description,
+                               PersonSvc director,
+                               List<PersonSvc> actors,
+                               Genre genre,
+                               PriceSvc price) {
 
-@AllArgsConstructor
-@Getter
-public class MovieSvcResponse {
-    private final String id;
-    private final String title;
-    private final String description;
-    private final PersonSvc director;
-    private final List<PersonSvc> actors;
-    private final Genre genre;
-    private final PriceSvc price;
 }
