3f13fccedc433cf53da6e395e23967f4f959ab8f
Fabio Borriello <fborriello@expediagroup.com>
2025-11-27 11:51:03 +0100

    removes an unused import

--- dao/src/test/java/com/expediagroup/dao/MovieDaoTest.java
+++ dao/src/test/java/com/expediagroup/dao/MovieDaoTest.java
@@ -5,7 +5,6 @@ import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
