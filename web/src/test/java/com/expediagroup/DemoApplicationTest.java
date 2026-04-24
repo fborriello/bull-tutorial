12f1da26906b8ead65c20af22fa5438548e5bf39
Fabio Borriello <borriello.fabio@gmail.com>
2026-04-24 11:59:55 +0200

    increased coverage

--- web/src/test/java/com/expediagroup/DemoApplicationTest.java
+++ web/src/test/java/com/expediagroup/DemoApplicationTest.java
@@ -5,12 +5,18 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTest {
 
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void mainMethodStartsApplication() {
+        DemoApplication.main(new String[] {});
     }
 
 }
